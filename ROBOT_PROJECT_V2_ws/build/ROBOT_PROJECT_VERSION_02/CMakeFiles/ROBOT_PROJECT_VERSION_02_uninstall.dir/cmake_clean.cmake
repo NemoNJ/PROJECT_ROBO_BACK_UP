779e6c0b0d666339de1aacbc54e3923be5545977
NemoNJ <nemojirayu>
2025-02-27 18:53:42 +0700

    Initial commit

---
+++ ROBOT_PROJECT_V2_ws/build/ROBOT_PROJECT_VERSION_02/CMakeFiles/ROBOT_PROJECT_VERSION_02_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ROBOT_PROJECT_VERSION_02_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ROBOT_PROJECT_VERSION_02_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
